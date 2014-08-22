1435137b63309db6eda49affe926197928bb0bd9
jbrouhard98 <jbrouhard@gmail.com>
2014-08-22 07:56:05 -0500

    Updating master index.php

--- index.php
+++ index.php
@@ -19,50 +19,13 @@
 
 // include configuration file
 
-if (isset($_SESSION['username']) && isset($_SESSION['token'])){
+if (isset($_SESSION['username'])){
         // lets send them to the main
         header('Location:/main');
     }
 
     else{
         //Display login form
-    
-
+		header('Location:/auth/');
+    }
 ?>
-
-<!DOCTYPE html>
-<!--[if lt IE 7]> <html class="lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
-<!--[if IE 7]> <html class="lt-ie9 lt-ie8" lang="en"> <![endif]-->
-<!--[if IE 8]> <html class="lt-ie9" lang="en"> <![endif]-->
-<!--[if gt IE 8]><!--> <html lang="en"> <!--<![endif]-->
-<head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-  <title>Dark Login Form</title>
-  <link rel="stylesheet" href="/assets/css/login.css">
-  <!--[if lt IE 9]><script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
-</head>
-<body>
-  <form method="post" action="login.php" class="login">
-    <p>
-      <label for="login">Email:</label>
-      <input type="text" name="username" id="username" value="username">
-    </p>
-
-    <p>
-      <label for="password">Password:</label>
-      <input type="password" name="password" id="password" value="password">
-    </p>
-
-    <p class="login-submit">
-      <button type="submit" class="login-button">Login</button>
-    </p>
-
-    <p class="forgot-password"><a href="#">Forgot your password?</a></p>
-  </form>
-
-</body>
-</html>
-
-<?php
-}
